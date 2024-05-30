3c4cba43ac85569bd5326d0353db442c837e376f
okkez <okkez000@gmail.com>
2024-05-31 06:54:16 +0900

    Add more test

--- post-search.sh
+++ post-search.sh
@@ -5,5 +5,12 @@ curl -X POST \
      --data-urlencode "working_directory=$(pwd)" \
      --data-urlencode "command=echo this is test" \
      http://localhost:8088
-
+echo
 curl http://localhost:8088
+
+echo
+echo -----
+
+curl http://localhost:8088/2
+echo
+
